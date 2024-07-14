47d4cf41aab22246a4fec8945fc0c4257d28a0a6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-14 18:11:02 +0100

    Add loadHeaderContent to DOM loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
+import { loadHeaderContent } from "./header";
 import { loadHomeContent } from "./home";
 
 document.addEventListener("DOMContentLoaded", () => {
+    loadHeaderContent();
     loadHomeContent();
 });
