84b3d5f5560aa863d34803e60f1890f2f1f436b0
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-15 14:12:09 +0100

    Add nav button event listeners

--- src/index.js
+++ src/index.js
@@ -5,4 +5,24 @@ import { loadHomeContent } from "./home";
 document.addEventListener("DOMContentLoaded", () => {
     loadHeaderContent();
     loadHomeContent();
+
+    const home = document.querySelector("#home");
+    home.addEventListener("click", () => {
+    loadHeaderContent();
+    loadHomeContent();
+    });
+
+    const menu = document.querySelector("#menu");
+    menu.addEventListener("click", () => {
+        loadHeaderContent();
+        loadMenuContent();
+    });
+
+    const about = document.querySelector("#about");
+    about.addEventListener("click", () => {
+        loadHeaderContent();
+        loadAboutContent();
+    });
+
 });
+
