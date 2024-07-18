a1f0a9673128215cc9dc6336b0332a05c37e9ef8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-18 12:52:18 +0100

    Refactor header contents from html layout to use js DOM

--- src/header.js
+++ src/header.js
@@ -2,15 +2,34 @@ import pizzaLogo from "./images/pizza-logo.png";
 
 export function loadHeaderContent() {
     const header = document.querySelector("header");
-    header.innerHTML = `
-        <div class="logo">
-                <img src="${pizzaLogo}" alt="Pizza logo">
-        </div>
-            <nav>
-                <button id="home">Home</button>
-                <button id="menu">Menu</button>
-                <button id="about">About</button>
-            </nav>    
-    `;    
+    header.innerHTML = "";
+
+    const logoContainer = document.createElement("div");
+    logoContainer.classList.add("logo");
+    header.appendChild(logoContainer);
+
+    const logo = document.createElement("img");
+    logo.src = pizzaLogo;
+    logo.alt = "Pizza Logo";
+    logoContainer.appendChild(logo);
+
+    const navBar = document.createElement("nav");
+    header.appendChild(navBar);
+
+    const homeButton = document.createElement("button");
+    homeButton.id = "home";
+    homeButton.textContent = "Home";
+
+    const menuButton = document.createElement("button");
+    menuButton.id = "menu";
+    menuButton.textContent = "Menu";
+
+    const aboutButton = document.createElement("button");
+    aboutButton.id = "about";
+    aboutButton.textContent = "About";
+
+    navBar.appendChild(homeButton);
+    navBar.appendChild(menuButton);
+    navBar.appendChild(aboutButton);
 }
 
